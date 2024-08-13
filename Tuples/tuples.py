26b583bba5816bb394e1a42b4925ce230b8a183b
iJoyRide <sumfeis3@gmail.com>
2024-08-13 17:20:30 +0800

    Tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,13 @@
+myTuple = ("Max", 28, "America", "Boston")
+
+print(type(myTuple))
+print("--------------------------------")
+
+item = myTuple[0]
+print(f"My name is {item}")
+print("--------------------------------")
+
+
+name, age, country, city = myTuple
+
+print(f"Hi my name is {name} and I am {age} years old. I came from {country}, {city}")
